ab93d6dc86896cc54b0d75e34da03f5c5f27afd0
oRonold <or040918@gmail.com>
2024-11-13 13:47:29 -0300

    Initial commit

---
+++ src/test/java/com/fiap/gs/energyEfficient/EnergyEfficientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.gs.energyEfficient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyEfficientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
