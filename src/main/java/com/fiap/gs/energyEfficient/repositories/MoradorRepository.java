a1a744b0784eb22682c2c56c92e62ad5624c8294
oRonold <or040918@gmail.com>
2024-11-14 14:36:27 -0300

    Polindo querys

--- src/main/java/com/fiap/gs/energyEfficient/repositories/MoradorRepository.java
+++ src/main/java/com/fiap/gs/energyEfficient/repositories/MoradorRepository.java
@@ -3,13 +3,16 @@ package com.fiap.gs.energyEfficient.repositories;
 import com.fiap.gs.energyEfficient.model.morador.Morador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface MoradorRepository extends JpaRepository<Morador, Long> {
 
+    @Transactional(readOnly = true)
     Optional<Morador> findByDadosMoradorCpf(String cpf);
 
+    @Transactional(readOnly = true)
     @Query("select m from Morador m where m.dadosMorador.cpf = ?1")
     Morador recuperarMoradorPorCpf(String cpf);
 
