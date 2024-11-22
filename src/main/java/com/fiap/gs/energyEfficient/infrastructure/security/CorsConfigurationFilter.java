56fdd6b983e6a51a84fe10fc121c9d5796e785b3
oRonold <or040918@gmail.com>
2024-11-22 19:01:39 -0300

    Atualizando CORS

--- src/main/java/com/fiap/gs/energyEfficient/infrastructure/security/CorsConfigurationFilter.java
+++ src/main/java/com/fiap/gs/energyEfficient/infrastructure/security/CorsConfigurationFilter.java
@@ -15,7 +15,9 @@ public class CorsConfigurationFilter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:8080",
+                "https://gs-savewatts-app.azurewebsites.net"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
